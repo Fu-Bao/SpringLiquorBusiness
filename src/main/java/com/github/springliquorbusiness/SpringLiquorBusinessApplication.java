42ad52a34a6276fa3dd84db93290a45002baaa08
Hi <iamjsjs0319@gmail.com>
2024-04-15 21:45:08 +0900

    init

---
+++ src/main/java/com/github/springliquorbusiness/SpringLiquorBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.github.springliquorbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquorBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquorBusinessApplication.class, args);
+    }
+
+}
